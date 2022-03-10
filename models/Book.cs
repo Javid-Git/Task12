7d4a37ae643f6a50e553c032e61c3d85be1296f3
Javid-Git <javidaal@code.edu.az>
2022-03-10 20:24:18 +0400

    Add project files.

---
+++ models/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12.models
+{
+    class Book
+    {
+        private static int _count=0;
+        public string Code { get; set; }
+        private string _name;
+        public string Name 
+        {
+            get => _name;
+            set
+            {
+                while(value.Length <= 3)
+                {
+                    Console.WriteLine("Kitabin adi 3 herfden cox olmalidir!");
+                    value = Console.ReadLine();
+                }
+                _name = value;
+            }
+        }
+        public string AuthorName { get; set; }
+        public int PageCount { get; set; }
+
+        public Book(string name, string authorname, int pagecount)
+        {
+            Name = name;
+            AuthorName = authorname;
+            PageCount = pagecount;
+            _count++;
+            Code = $"{Name.Substring(0,2).ToUpper()}{_count}";
+        }
+    }
+}
---
+++ models/Library.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12.models
+{
+    class Library
+    {
+        List<Book> books;
+
+        public Library()
+        {
+            books = new List<Book>();
+        }
+
+        public List<Book> FindAllBooksByName(string item)
+        {
+            List<Book> booknames = new List<Book>();
+            foreach (Book book in books)
+            {
+                if (book.Name.Contains(item))
+                {
+                    booknames.Add(book);
+                }
+            }
+            if (booknames.Count == 0)
+            {
+                Console.WriteLine($"Yazdiginiz '{item}' parametri uzre bir kitab tapilmadi!");
+            }
+            return booknames;
+        }
+        public void RemoveAllBookByName(string item)
+        {
+            foreach (Book book in books)
+            {
+                if (book.Name.Contains(item))
+                {
+                    books.Remove(book);
+                    Console.WriteLine($"{book.Name} kitabi silindi!");
+                    return;
+                }
+                Console.WriteLine($"Yazdiginiz '{item}' parametri uzre bir kitab tapilmadi!");
+            }
+        }
+        public List<Book> SearchBooks(string item)
+        {
+            List<Book> searchedbooks = new List<Book>();
+            foreach (Book book in books)
+            {
+                if (book.Name.Contains(item))
+                {
+                    searchedbooks.Add(book);
+                }
+                else if (book.AuthorName.Contains(item))
+                {
+                    searchedbooks.Add(book);
+                }
+                else if (book.PageCount.ToString().Contains(item))
+                {
+                    searchedbooks.Add(book);
+                }
+            }
+            if (searchedbooks.Count == 0)
+            {
+                Console.WriteLine($"Yazdiginiz '{item}' parametri uzre bir kitab tapilmadi!");
+            }
+            return searchedbooks;
+        }
+        public List<Book> FindAllBooksByPageCountRange(int item1, int item2)
+        {
+            List<Book> booksbypage = new List<Book>();
+            foreach (Book book in books)
+            {
+                if (book.PageCount>item1 && book.PageCount < item2 )
+                {
+                    booksbypage.Add(book);
+                }
+            }
+            if (booksbypage.Count == 0)
+            {
+                Console.WriteLine($"Yazdiginiz '{item1}' ve '{item2}' sehife araliqlarinda olan kitab tapilmadi!");
+            }
+            return booksbypage;
+        }
+        public void RemoveByNo(string item)
+        {
+            foreach (Book book in books)
+            {
+                if (book.Code == item.ToUpper())
+                {
+                    books.Remove(book);
+                    Console.WriteLine($"{item.ToUpper()} nomreli kitab silindi!");
+                    return;
+                }
+            }
+            Console.WriteLine($"Axtardiginiz {item.ToUpper()} nomre uzre kitab tapilmadi!");
+        }
+
+    }
+}
